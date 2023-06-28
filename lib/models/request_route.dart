ffdd51ccee4fd9a1e416c5a578168808e4e2c465
margarote <joaovitortur1@gmail.com>
2023-06-27 22:29:07 -0300

    add

--- lib/models/request_route.dart
+++ lib/models/request_route.dart
@@ -1,6 +1,3 @@
-import 'dart:async';
-
-// ignore: depend_on_referenced_packages
 import 'package:shelf/shelf.dart';
 
 import '../enum/enum_route_type.dart';
@@ -8,7 +5,7 @@ import '../enum/enum_route_type.dart';
 class RequestRoute {
   final RouteType routeType;
   final String path;
-  final FutureOr<Response> Function(Request request) request;
+  final Function request;
 
   RequestRoute({required this.routeType, required this.path, required this.request});
 }
